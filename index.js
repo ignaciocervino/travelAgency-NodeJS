f7737c3c8ab345dbeaac9809d977ac3b05b8d9fb
ignaciocervino <ignaciocervino@gmail.com>
2021-07-03 15:59:54 -0300

    Changing to import version
    Changing from common js to import

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const express = require('express');
+// const express = require('express');//Common JS no propia de js
+import express from 'express';//Version de imports
 
 const app = express();
 
 //Definir puerto
 const port  = process.env.PORT || 4000;
+
+
+app.get('/', (req,res)=>{//request es lo que yo envio, response es lo que express me responde
+    res.send('Inicio');
+});
 
 app.listen (()=>{
     console.log(`El Servidor esta funcionando en el puerto ${port}`);
