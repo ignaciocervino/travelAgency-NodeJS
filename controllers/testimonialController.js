9997e9114b93b981e05f8bfdab0479ced2fefe04
ignaciocervino <ignaciocervino@gmail.com>
2021-07-04 16:13:12 -0300

    Display testimonials

--- controllers/testimonialController.js
+++ controllers/testimonialController.js
@@ -19,14 +19,16 @@ const guardarTestimonial = async (req,res) =>{
     }
 
     if (errores.length > 0) {
-        
+        //Consultar testimoniales existentes
+        const testimoniales = await Testimonial.findAll();
         //Mostrar la vista con errores
         res.render('testimoniales',{
             pagina: 'Testimoniales',
             errores,
             nombre,
             correo,
-            mensaje
+            mensaje,
+            testimoniales
         });
     }
     else{
