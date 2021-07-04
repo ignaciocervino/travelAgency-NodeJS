8cadbec14e7291064879e88f2a782e9e1a5183d3
ignaciocervino <ignaciocervino@gmail.com>
2021-07-04 15:49:59 -0300

    Adding more validation to the form

--- controllers/testimonialController.js
+++ controllers/testimonialController.js
@@ -1,22 +1,36 @@
 
 const guardarTestimonial = (req,res) =>{
     //Validar formulario
-
     const {nombre,correo,mensaje} = req.body;
     const errores = [];
     if (nombre.trim() === '') {
-        errores.push({nombre: `El nombre esta vacio`});
+        errores.push({mensaje: `El nombre esta vacio`});
         
     }
 
     if (correo.trim() === '') {
-        errores.push({correo: `El correo esta vacio`});
+        errores.push({mensaje: `El correo esta vacio`});
         
     }
 
     if (mensaje.trim() === '') {
         errores.push({mensaje: `El mensaje esta vacio`});
         
+    }
+
+    if (errores.length > 0) {
+        
+        //Mostrar la vista con errores
+        res.render('testimoniales',{
+            pagina: 'Testimoniales',
+            errores,
+            nombre,
+            correo,
+            mensaje
+        });
+    }
+    else{
+        //Almacenarlo en la base de datos
     }
 }
 
