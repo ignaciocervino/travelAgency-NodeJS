8b524fb1f71205173299f42083aaf5c2657fa9da
ignaciocervino <ignaciocervino@gmail.com>
2021-07-04 11:57:46 -0300

    Adding more views

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,30 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req,res)=>{//request es lo que yo envio, response es lo que express me responde
-    res.render('Inicio');
+    res.render('Inicio',{
+        pagina: 'Inicio',
+    });
 });
 
 router.get('/nosotros', (req,res)=>{//request es lo que yo envio, response es lo que express me responde
     
-    res.render('nosotros');
+    res.render('nosotros', {
+        pagina: 'Nosotros'
+    });
+});
+
+router.get('/viajes', (req,res)=>{//request es lo que yo envio, response es lo que express me responde
+    
+    res.render('viajes', {
+        pagina: 'Viajes'
+    });
+});
+
+router.get('/testimoniales', (req,res)=>{//request es lo que yo envio, response es lo que express me responde
+    
+    res.render('testimoniales', {
+        pagina: 'Testimoniales'
+    });
 });
 
 
